f080e2f20ec215823cba3e23b43fa03afe8e993a
HellerDeveller <150496986+HellerDeveller@users.noreply.github.com>
2024-09-29 19:08:27 -0400

    Add files via upload

--- node-crash-course/app.js
+++ node-crash-course/app.js
@@ -3,30 +3,77 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
+//connect to mongoDB, still gives deprecation warning...?
+//and is where you connect to localhost 3000
+const dbURI = "mongodb+srv://some_idiot:test1235@nodetuts.bwba1.mongodb.net/nodetuts?retryWrites=true&w=majority&appName=nodetuts";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 //register view engine
 app.set('view engine', 'ejs');
 //app.set('views', 'myviews');
 
-//make localhost
-app.listen(3000);
-
 //middleware (next is undefined...? whatever, this works)
 //oh and uh, static files: it looks in a folder called 'public'
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+
+// mongoose and mongo sanbox routes, put /add-blog or /single-blog in url and see what happens!
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about me blog',
+        body: 'so about my blogogog'
+    });
+
+    blog.save()
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById("66f9ce941b1a8214d831d1fb")
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+})
 
 //home page
 app.get('/', (req, res) => {
     //res.sendFile('./views/index.html', {root: __dirname});
 
 //take away blogs to see the 'there are no blogs' message!
+/*
     const blogs = [
         {title: 'Awooga', snippet: 'They do that sometimes ;)'},
         {title: 'Help', snippet: 'The guy above me is a creep :<'}
     ];
 
     res.render('index', { title: 'Home', blogs });
+*/
+    res.redirect('/blogs');
 });
 
 //about page
@@ -39,6 +86,54 @@ app.get('/about', (req, res) => {
 //app.get('/about-us', (req, res) => {
     //res.redirect('/about');
 //});
+
+//blog routes (see home page), also sorts them by newest
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ creatdAt: -1 })
+    .then((result) => {
+        res.render('index', { title: 'All Blogs', blogs: result });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+//make your blogs come true WOOOO (also check middleware, it requires
+//app.use(express.urlencoded({ extended: true })); to work properly)
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+    .then((result) => {
+        res.redirect('/blogs');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+    .then(result => {
+        res.render('details', { blog: result , title: 'Blog Details' });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
+//deleting your own hard work (blogs)
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs' });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
 
 //create blogs EJS STYLE!!!
 app.get('/blogs/create', (req, res) => {
